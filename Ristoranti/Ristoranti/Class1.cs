e279cf18648e3424bfddf5c0790f24612be105ca
Cereda12 <Gabrycereda@gmail.com>
2020-05-22 14:48:47 +0200

    WF

---
+++ Ristoranti/Ristoranti/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ristoranti
+{
+    public struct Ristoranti
+    {
+        public string nome;
+        public string zona;
+        public int posti;
+        public string tipo;
+        public string struttura;
+        public string img;
+    }
+
+    class Class1
+    {
+
+
+    }
+}
